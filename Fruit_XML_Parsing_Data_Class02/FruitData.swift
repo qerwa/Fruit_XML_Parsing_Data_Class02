a2632b2322842b796f05e1f77c3f7d27e7ba6090
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-10-01 15:56:37 +0900

    create

---
+++ Fruit_XML_Parsing_Data_Class02/FruitData.swift
@@ -0,0 +1,14 @@
+//
+//  FruitData.swift
+//  Fruit_XML_Parsing_Data_Class02
+//
+//  Created by D7703_17 on 2018. 10. 1..
+//  Copyright © 2018년 D7703_17. All rights reserved.
+//
+
+import Foundation
+class FruitData {
+      var name = ""
+      var color = ""
+      var cost = ""
+}
--- Fruit_XML_Parsing_Data_Class02/ViewController.swift
+++ Fruit_XML_Parsing_Data_Class02/ViewController.swift
@@ -8,13 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, XMLParserDelegate {
 
+      
+      //데이터 객체 초기화
+      var myFruitData = [FruitData]()
+      var fName = ""
+      var fColor = ""
+      var fCost = ""
+      
+      var currentElement = ""
+      
+      
       override func viewDidLoad() {
             super.viewDidLoad()
             // Do any additional setup after loading the view, typically from a nib.
+            if let path = Bundle.main.url(forResource: "Fruit", withExtension: "xml") {
+                  if let myParser = XMLParser(contentsOf: path) {
+                        myParser.delegate = self
+                        
+                        // 파싱 시작
+                        if myParser.parse() {
+                              print("Parsing succeed")
+//                              print(myFruitData[0].name)
+//                              print(myFruitData[0].color)
+//                              print(myFruitData[0].cost)
+                        } else {
+                              print("Parsing failed")
+                        }
+                  } else {
+                        print("path error")
+                  }
+            } else {
+                  print("file error")
+            }
       }
-
-
+      // XMLParserDelegate 메소드 호출
+      // <tag>를 만날때
+      func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+            currentElement = elementName
+            print(currentElement)
+      }
+      // tag 다음에 문자열을 만날때
+      func parser(_ parser: XMLParser, foundCharacters string: String) {
+            //공백제거
+            let data = string.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+            
+            if !data.isEmpty {
+                  switch currentElement {
+                  case "name" : fName = data
+                  case "color" : fColor = data
+                  case "cost" : fCost = data
+                  default : break
+                  }
+            }
+      }
+      
+      // </tag>를 만날때
+      func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+            if elementName == "item" {
+                  let myItem = FruitData()
+                  myItem.name = fName
+                  myItem.color = fColor
+                  myItem.cost = fCost
+                  myFruitData.append(myItem)
+            }
+      }
 }
-
